0c138c036b614447861d54d5de735963e8deb80d
sk-repository <34868573+sk-repository@users.noreply.github.com>
2022-05-23 09:09:41 +0200

    Update ccdGenerator.py

--- ccdGenerator.py
+++ ccdGenerator.py
@@ -1,7 +1,10 @@
 import csv
 
+CFG_NAME = 0
+IP_ADDR = 1
+
 with open('certList.csv', 'r') as file:
     csvreader = csv.reader(file)
     for row in csvreader:
-        with open('ccd_files/' + row[0], 'w') as ccd:
-            ccd.write('ifconfig-push ' + row[1] + ' 255.255.255.0')
+        with open('ccd_files/' + row[CFG_NAME], 'w') as ccd:
+            ccd.write('ifconfig-push ' + row[IP_ADDR] + ' 255.255.255.0')
