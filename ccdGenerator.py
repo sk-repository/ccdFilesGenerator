7cf8799ad7609108975edbba63409d9ad25b2b2f
sk-repository <34868573+sk-repository@users.noreply.github.com>
2022-05-23 09:08:49 +0200

    Update ccdGenerator.py

--- ccdGenerator.py
+++ ccdGenerator.py
@@ -5,5 +5,3 @@ with open('certList.csv', 'r') as file:
     for row in csvreader:
         with open('ccd_files/' + row[0], 'w') as ccd:
             ccd.write('ifconfig-push ' + row[1] + ' 255.255.255.0')
-            ccd.close()
-    file.close()
